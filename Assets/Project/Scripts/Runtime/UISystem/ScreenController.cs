adf0f1fd46de23a1eff1c7ec23a9b76987d81f9a
Iván Schroeder <ivanschroeder99@outlook.com>
2025-10-07 02:13:16 -0300

    Project Structure Cleaning

---
+++ Assets/Project/Scripts/Runtime/UISystem/ScreenController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityUtilities;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+
+[RequireComponent(typeof(Canvas))]
+[RequireComponent(typeof(CanvasGroup))]
+public class ScreenController : MonoBehaviour {
+    public string ScreenName = "";
+    private Canvas CanvasComponent;
+    private CanvasGroup CanvasGroupComponent;
+    public bool IsShown = false;
+
+    void Awake() {
+        if (CanvasComponent.IsNull()) CanvasComponent = GetComponent<Canvas>();
+        CanvasGroupComponent = GetComponent<CanvasGroup>();
+    }
+
+    public void ShowScreen() => SetCanvasGroupAlpha(1);
+    public void HideScreen() => SetCanvasGroupAlpha(0);
+
+    public void EnableInteractability() => SetCanvasGroupState(true);
+    public void DisableInteractability() => SetCanvasGroupState(false);
+
+    private void SetCanvasGroupState(bool state) {
+        CanvasGroupComponent.interactable = state;
+        CanvasGroupComponent.blocksRaycasts = state;
+    }
+
+    private void SetCanvasGroupAlpha(float alpha) {
+        CanvasGroupComponent.alpha = alpha;
+        IsShown = alpha == 1 ? true : false;
+        CanvasComponent.enabled = IsShown;
+    }
+}
